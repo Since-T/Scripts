e9bb2fa9a6506b8bb09f054285c4400ea5028d5c
Since <tammo.reinken@ewetel.net>
2020-06-24 12:43:03 +0200

    Update

--- Since Kalista/header.lua
+++
@@ -1,9 +0,0 @@
-return {
-    riot=true;
-    id = 'SinceKalista';
-    name = 'Since Kalista';
-    type = "Champion";
-    load = function()
-      return player.charName == "Kalista"
-    end;
-}
--- Since KogMaw/header.lua
+++
@@ -1,9 +0,0 @@
-return {
-    riot=true;
-    id = 'SinceKogMaw';
-    name = 'Since KogMaw';
-    type = "Champion";
-    load = function()
-      return player.charName == "KogMaw"
-    end;
-}
